094f1a9126db695ebe7d0a3be0afb0f1f465a1db
Fujii Makoto <147135481+makoty-pg@users.noreply.github.com>
2025-03-11 21:09:19 +0900

    ボトムナビゲーションバー追加（追加に伴い，main.dartいじりまくりました．clockページは，clock_home.dartに入ってます）

---
+++ lib/quiz_home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class QuizHome extends StatelessWidget {
+  const QuizHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('メモクロ'),
+        backgroundColor: Colors.purple,
+      ),
+      body: null,
+    );
+  }
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:memorization_and_clock/main.dart';
+import 'package:memorization_and_clock/route.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
