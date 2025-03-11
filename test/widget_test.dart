6ef13fc478239d2ab01ac2a19707f10737fb390c
Fujii Makoto <147135481+makoty-pg@users.noreply.github.com>
2025-03-11 21:10:00 +0900

    ボトムナビゲーションバー追加（追加に伴い，main.dartいじりまくりました．clockページは，clock_home.dartに入ってます）

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:memorization_and_clock/main.dart';
 import 'package:memorization_and_clock/route.dart';
 
 void main() {
