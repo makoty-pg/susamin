a39b657601912b92af715566a401e8aa1ced4c8f
Fujii Makoto <147135481+makoty-pg@users.noreply.github.com>
2025-03-22 20:20:16 +0900

    結果画面作成 問題集通常モード（isTest=false）と，アラームモード（isTest=true）に分け，画面遷移を条件分岐した

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:memorization_and_clock/quiz_screen.dart';
 
 class ResultScreen extends StatefulWidget {
-  ResultScreen({super.key, required this.result, required this.length, required this.isTest, required this.alarmId});
+  const ResultScreen({super.key, required this.result, required this.quizMap, required this.isTest, required this.alarmId});
   final int result;
-  final int length;
+  final List<Map> quizMap;
   final bool isTest;
   final int alarmId;
 
@@ -12,9 +13,65 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+  late String comment;
+
+  Future<void> goToScreen(BuildContext context, int rate) async {
+    if (widget.isTest) {
+      if (rate >= 80.0) {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => QuizScreen(
+              quizMap: widget.quizMap,
+              isTest: widget.isTest, alarmId: widget.alarmId,
+            ),
+          ),
+        );
+
+      }
+    } else {
+      Navigator.of(context).popUntil((route) => route.isFirst);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    int rate = widget.result.round() * 100 ~/ widget.quizMap.length;
+    String buttonText;
+
+    if (widget.isTest && rate < 80.0) {
+      buttonText = "もう一度";
+    } else if (widget.isTest && rate >= 80.0){
+      buttonText = "アラーム停止";
+    } else {
+      buttonText = "トップへ戻る";
+    }
+
+    if (rate >= 80.0) {
+      comment = "合格！";
+    }else{
+      comment = "不合格";
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          //Columnの中に入れたものは縦に並べられる．Rowだと横に並べられる
+          mainAxisAlignment: MainAxisAlignment.center, //Coloumの中身を真ん中に配置
+          children: <Widget>[
+            Text(comment),
+            Text('正答数${widget.result}'),
+            Text('正答率$rate%'),
+            ElevatedButton(
+                onPressed: () async {
+                  await goToScreen(context, rate);
+                },
+                child: Text(buttonText)),
+          ],
+        ),
+      ),
+    );
   }
 }
